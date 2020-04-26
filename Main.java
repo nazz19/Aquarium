556b722a3460f40219dd595f6f0e118f513b49a9
nazz19 <63659661+nazz19@users.noreply.github.com>
2020-04-26 23:15:19 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Aquarium aquarium = new Aquarium();
+        aquarium.addSpecies();
+        aquarium.changeSize();
+        aquarium.changeAge();
+        aquarium.printAllSpecies();
+    }
+}
